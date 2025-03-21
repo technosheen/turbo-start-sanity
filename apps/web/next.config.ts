011137a7455c3ebff75a7225a53f118730462a29
hrithikroboto <hrithik@robotostudio.com>
2025-03-21 12:13:09 +0530

    chore: update package versions and configuration settings

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   transpilePackages: ["@workspace/ui"],
   experimental: {
     reactCompiler: true,
-    ppr: true,
+    // ppr: true,
     inlineCss: true,
   },
   logging: {
